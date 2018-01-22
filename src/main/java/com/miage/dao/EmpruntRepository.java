becd8a407da77f28b700c619c9e7747b47b95739
rezguiHamza <rezgui.ing@gmail.com>
2018-01-22 14:12:32 +0100

    update operation emprunt retour

--- src/main/java/com/miage/dao/EmpruntRepository.java
+++ src/main/java/com/miage/dao/EmpruntRepository.java
@@ -1,16 +1,20 @@
 package com.miage.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import com.miage.entities.Client;
 import com.miage.entities.Emprunt;
-
+import com.miage.entities.Media;
  
 
 public interface EmpruntRepository extends JpaRepository<Emprunt, Long>{
 	
-//	@Query("select e.media  from Emprunt e   where    and e.client=:x ")
-//	Page<Media> consultermVideoEmpBy(@Param("x")Client client, Pageable pageable);
+	@Query("select e.media  from Emprunt e   where e.client=:x ")
+	Page<Media> consulterMediaEmp(@Param("x")Client client, Pageable pageable);
 
 	
 
---
+++ src/main/java/com/miage/metier/IEmpruntMetier.java
@@ -0,0 +1,5 @@
+package com.miage.metier;
+
+public interface IEmpruntMetier {
+
+}
